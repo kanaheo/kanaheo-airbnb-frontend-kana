57d39e2466d932c9398265191cb8f06c3f2d5f6f
kanaheo <stalvede0421@gmail.com>
2022-12-16 23:00:41 +0900

    api edit fetch -> axios

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 // 여기에는 우리가 api를 fetch하기 위해 적었던 모든 함수들을 가져온다 !
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+import axios from "axios";
 
-export async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
-}
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1/",
+});
+
+export const getRooms = () =>
+  instance.get("rooms/").then((response) => response.data);
