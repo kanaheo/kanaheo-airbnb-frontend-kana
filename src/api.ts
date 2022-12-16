8ac8501694027331eb761ec7acb4c4a02d0d7dec
kanaheo <stalvede0421@gmail.com>
2022-12-17 00:05:38 +0900

    roomdetail api end

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 // 여기에는 우리가 api를 fetch하기 위해 적었던 모든 함수들을 가져온다 !
 import axios from "axios";
+import { QueryFunctionContext } from "@tanstack/react-query";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1/",
@@ -8,5 +9,7 @@ const instance = axios.create({
 export const getRooms = () =>
   instance.get("rooms/").then((response) => response.data);
 
-export const getRoom = () =>
-  instance.get(`rooms/1`).then((response) => response.data);
+export const getRoom = ({ queryKey }: QueryFunctionContext) => {
+  const [_, roomPk] = queryKey;
+  instance.get(`rooms/${roomPk}`).then((response) => response.data);
+};
