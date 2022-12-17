f0e2d493e09836684f83720fe6b1a8e08c7a0a2d
kanaheo <stalvede0421@gmail.com>
2022-12-17 21:03:56 +0900

    backend url same & request cookie problem fix

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import { QueryFunctionContext } from "@tanstack/react-query";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1/",
+  withCredentials: true,
 });
 
 export const getRooms = () => {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -71,7 +71,7 @@ export default function Header() {
               </LightMode>
             </>
           ) : (
-            <Avatar size={"md"} />
+            <Avatar name={user.name} src={user.avatar} size={"md"} />
           )
         ) : (
           <>
